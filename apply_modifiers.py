1a686cea9e7a298bf2d211863484f8cd595c8916
Carlsu~ <104013959+carls3d@users.noreply.github.com>
2023-07-17 16:11:17 +0200

    Fixed apply_modifers not working properly in 3.5

--- apply_modifiers.py
+++ apply_modifiers.py
@@ -1,21 +1,28 @@
 import bpy
-import timeit
 
 # By Carlsu https://github.com/carls3d
-# Made to be injected as a script in a class
+# Made to be injected as a script in a serpens class
 
-def applyModifiers():
+def applyModifiers(modifierName:str = None):
+    def select_object(ob:bpy.types.Object, select:bool):
+        if bpy.app.version < (3, 5, 0):
+            ob.select = True
+        else:
+            ob.select_set(True)
     obj = bpy.context.active_object
     shapekeys = obj.data.shape_keys.key_blocks
-    # When ran on it's own as a script
-    if 'modifierName' not in locals():
-        modifierName = obj.modifiers.active.name
-        
-    obj_keys = []
+    # Use active when ran on it's own as a script
+    if not modifierName and obj.modifiers.active:
+        if obj.modifiers.active:
+            
+            modifierName = obj.modifiers.active.name
+        else: return
+    
+    objects = []
     for i, k in enumerate(shapekeys[1:]):
         bpy.context.view_layer.objects.active = obj
         bpy.ops.object.select_all(action='DESELECT')
-        obj.select = True
+        select_object(obj, True)
         bpy.ops.object.duplicate()
         
         bpy.context.active_object.show_only_shape_key = True        # Only use a single shapekey value
@@ -23,21 +30,20 @@ def applyModifiers():
         bpy.ops.object.shape_key_remove(all=True, apply_mix=True)   # Apply shapekey
         bpy.ops.object.modifier_apply(modifier=modifierName)        # Apply modifier
         bpy.context.active_object.name = k.name                     # Set name
-        obj_keys.append(bpy.context.active_object)                  # Add to list
+        objects.append(bpy.context.active_object)                  # Add to list
 
-    for k in obj_keys: k.select = True
+    for ob in objects: 
+        select_object(ob, True)
     bpy.context.view_layer.objects.active = obj             # Set active object
     bpy.context.active_object.shape_key_clear()             # Remove shapekeys
     bpy.ops.object.modifier_apply(modifier=modifierName)    # Apply modifier
     bpy.ops.object.join_shapes()                            # Add shapekeys
-    for k in obj_keys:                                      # Delete placeholders
+    for k in objects:                                       # Delete placeholders
         bpy.data.meshes.remove(k.data)
-    obj.select = True
+    select_object(obj, True)
 
-if __name__ == '__main__' or 'modifierName' in locals():  
-    start = timeit.default_timer()
-    applyModifiers()
-    stop = timeit.default_timer()
-    print(f'Time: {stop-start}\n')
+# if __name__ == '__main__':  
+    # Use active modifier when ran as a script
+    # applyModifiers()
     
     
